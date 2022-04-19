64cc74f5acab569f55525d889ad7ca578c381faf
Pauline Schnoebelen <paulineschnoebelen@PaulineS.local>
2022-04-19 17:59:03 +0200

    add html

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ app.get('/', (req, res,) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/about', (req, res,) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
 
-app.listen(3001, () => {
+
+app.listen(3002, () => {
 console.log("first page view")
 });
